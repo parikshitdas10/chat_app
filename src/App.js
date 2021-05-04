eac91d936fc74a45de0ca0a1dd76dbd003f3532b
parikshit das <parikshitdas10@gmail.com>
2021-05-03 22:07:15 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="1b8bb68c-8a00-4e34-be9e-70d95086a93f"
+            userName="parikshitdas"
+            userSecret="9674823352"
+        
+        
+        />
+    )
+}
+
+
+export default App;
